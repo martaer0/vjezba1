cedcbdcbb5ef10948e3e6397d232beada8defd6f
martaer0 <162578383+martaer0@users.noreply.github.com>
2024-03-07 13:26:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ucenik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ucenik> Ucenici = new List<Ucenik>();
+            int n;
+
+            Console.Write("Unesi broj ucenika: ");
+            n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                Ucenik u = new Ucenik();
+                Ucenici.Add(u);
+            }
+
+            Ucenik Max = Ucenici[0];
+            foreach (Ucenik u in Ucenici)
+            {
+                if (u.Prosjek() > Max.Prosjek())
+                    Max = u;
+            }
+
+            Console.Write("Najbolji ucenik: ");
+            Max.Ispis();
+        }
+    }
+}
